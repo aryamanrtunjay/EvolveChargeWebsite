51dc2c4c7bc60169f5f30d6778e6fc9b909cdc6d
Decaaaa <104375071+Decaaaa@users.noreply.github.com>
2025-02-24 09:50:54 -0800

    WIP

--- evolve-charge/tailwind.config.mjs
+++ evolve-charge/tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +7,29 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'pioneer-orange': '#fdbd53',
+        'pioneer-dark-orange': '#cc7900'
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        '6xl': '3.815rem',
+        '7xl': '4.768rem',
+        '8xl': '5.96rem',
+        '9xl': '7.451rem',
+        '10xl': '9.313rem',
+      }
     },
   },
   plugins: [],
