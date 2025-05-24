348bce1c6017bb3a72765cdef096098514852ae7
Aryaman Rtunjay <aryamanrtunjay@gmail.com>
2025-05-24 00:58:44 -0700

    Replace with real stripe key references

--- evolve-charge/src/app/api/create-payment-intent/route.js
+++ evolve-charge/src/app/api/create-payment-intent/route.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_TEST_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
   try {
